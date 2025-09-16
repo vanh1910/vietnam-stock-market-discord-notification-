ff1335ad17f07f7ce0c7b734e9e1ab1725405ec2
akari <pikachungaytho@gmail.com>
2025-09-16 16:11:50 +0700

    create testing program

---
+++ test/test_api_handler.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from services.api_handler import api_handler
+
+def test_api_handler():
+    resolution = "10"
+    range = pd.Timedelta(2, "d")
+    ticker = "HPG"
+    handler = api_handler()
+    handler.fetch_realtime_data(resolution, range, ticker)
+
+if __name__ == "__main__":
+    test_api_handler()
